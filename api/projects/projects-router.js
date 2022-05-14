a7f2fd2d39f6bfa73825fb8ea274b86d25b519f5
Ricardorouge <ricky032929@gmail.com>
2022-05-14 14:04:42 -0400

    project get "/"

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,20 @@
 // Write your "projects" router here!
+const express = require('express')
+const Projects = require('./projects-model')
+
+const router = express.Router()
+
+router.get('/',(Req,res,next)=>{
+    Projects.get()
+    .then(result=>{
+        result?
+        res.json(result)
+        :
+        res.json([])
+    })
+    .catch(next)
+})
+
+
+
+module.exports = router
