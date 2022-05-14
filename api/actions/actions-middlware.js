1191c9e302cc5fb985261c659d00805e56fa2c21
Ricardorouge <ricky032929@gmail.com>
2022-05-14 15:14:48 -0400

    added validateID middleware to actions

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,21 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model')
+
+async function validateID(req,res,next){
+    try{
+        const action = await Actions.get(req.params.id)
+        !action?
+        res.status(404).json({message: ' no action found '})
+        :
+        req.action = action
+        next()
+
+    }catch(err){
+        res.status(500).end()
+    }
+} 
+
+module.exports = {
+    validateID,
+    
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,24 @@
 // Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model')
+const {validateID} = require('./actions-middlware')
+
+const router = express.Router()
+
+router.get('/',(req,res,next)=>{
+    Actions.get()
+    .then(action=>{
+        action?
+        res.json(action)
+        :
+        res.json([])
+    })
+    .catch(next)
+})
+
+router.get('/:id',validateID,(req,res,next)=>{
+    res.json(req.action)
+})
+
+
+module.exports = router
