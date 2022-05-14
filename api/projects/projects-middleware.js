11f80a1c786a98ba5d99ee9b7755c82f924cc04b
Ricardorouge <ricky032929@gmail.com>
2022-05-14 14:46:02 -0400

    created middleware for projects router and did router-delete

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,40 @@
 // add middlewares here related to projects
+
+const Projects = require('./projects-model')
+
+function validateProject(req,res,next){
+    const {name,description} = req.body
+    if(!name || !description){
+        res.status(400).json({message:'you must provide a name and description'})
+    } else{
+        next()
+    }
+}
+function validateUpdate(req,res,next){
+    const {name,description,completed} = req.body
+    if(!name || !description || completed==null){
+        res.status(400).json({message:'you must provide a name and description'})
+    } else{
+        next()
+    }
+}
+
+async function validateID(req,res,next){
+    try{
+        const project = await Projects.get(req.params.id)
+        !project?
+        res.status(404).json({message:'no project found'})
+        :
+        req.project=project
+        next()
+
+    }catch(err){
+        res.status(500).end()
+    }
+}
+
+module.exports = {
+    validateProject,
+    validateID,
+    validateUpdate
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,7 @@
 // Write your "projects" router here!
 const express = require('express')
 const Projects = require('./projects-model')
+const {validateProject,validateID,validateUpdate} = require('./projects-middleware')
 
 const router = express.Router()
 
@@ -15,6 +16,36 @@ router.get('/',(Req,res,next)=>{
     .catch(next)
 })
 
+router.get('/:id',validateID,(req,res,next)=>{
+   res.json(req.project)
+})
 
+router.post('/',validateProject,(req,res,next)=>{
+    Projects.insert(req.body)
+    .then(project=>{
+        res.json(project)
+    })
+    .catch(next)  
+})
+
+router.put('/:id',validateID,validateUpdate,(req,res,next)=>{
+    Projects.update(req.params.id,req.body)
+    .then(result=>{
+        res.json(result)
+    })
+    .catch(next)
+})
+
+router.delete('/:id',validateID,(req,res,next)=>{
+    Projects.remove(req.params.id)
+    .then(result=>{
+        res.end()
+    })
+    .catch(next)
+})
+
+router.get('/id/actions',(req,res,next)=>{
+    
+})
 
 module.exports = router
